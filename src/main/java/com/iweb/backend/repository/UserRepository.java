7e4a37247afcecb7e08d7589fd9c7bba0c4907ab
Isuru Piumal <isurupiumal@gmail.com>
2021-05-09 19:17:21 +0530

    website services

--- src/main/java/com/iweb/backend/repository/UserRepository.java
+++ src/main/java/com/iweb/backend/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.iweb.backend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.iweb.backend.models.User;
@@ -14,4 +16,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Boolean existsByUsername(String username);
 
 	Boolean existsByEmail(String email);
+
+	@Query("SELECT u FROM User u WHERE u.id = :id")
+	User findByI(@Param("id") Long id);
 }
